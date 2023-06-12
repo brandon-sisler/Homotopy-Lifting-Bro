a70a72853c6c01699787022350836a0bb3e9559f
Gary D Dunkerley <garydunkerley@brandeis.edu>
2023-06-12 21:48:42 +0000

    first attempt to define the cover

--- my_project/universal.lean
+++ my_project/universal.lean
@@ -1,1 +1,8 @@
 import Mathlib.Topology.Homotopy.Path
+open Set
+
+variable {X : Type _} [TopologicalSpace X] {x₀ x₁ : X }
+-- def baseSet : TopologicalSpace X 
+
+def Cover :=
+  ⋃ x₁ ∈ X , {γ | γ ∈ Path.Homotopic.Quotient x₀ x₁}
