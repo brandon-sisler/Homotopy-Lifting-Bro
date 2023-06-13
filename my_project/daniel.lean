b1d3f013bf1d1cd1b032641a2bbd0f74bf93f477
morrison-daniel <39346894+morrison-daniel@users.noreply.github.com>
2023-06-12 21:23:20 -0700

    collect notation

--- my_project/daniel.lean
+++ my_project/daniel.lean
@@ -13,15 +13,23 @@ variable (x : X) (y : Y) (t : I)
 
 
 /-
-Continuous f : f is globally continuous
-ContinuousAt f a : f is continuous at the point a
-
 Notation:
-E is is the covering space of X - otherwise denoted tilde X
-F is the lift of the map f
-F₀ is the start of F, i.e. F₀ = F(y, 0)
+`E` : the covering space of `X` - otherwise denoted tilde X
+`F` : the lift of the map `f`
+`F₀` : the start of `F`, i.e. `F₀ = F(y, 0)`
+
+`Continuous f` : `f` is globally continuous
+`ContinuousAt f a` : `f` is continuous at the point a
+
+`Trivialization F proj` : local trivialization of `proj : E → B` with fiber `F`
+`IsEvenlyCovered x ι` : `DiscreteTopology ι ∧ ∃ t : Trivialization ι p, x ∈ t.baseSet`
+                      : fiber over x has discrete topology & has a local trivialization
+`IsCoveringMap p` : `∀ x, IsEvenlyCovered (f x) (f ⁻¹' {x})`
+
+Theorems:
+`toTrivialization` : gets local trivialization above a point from a covering map
 -/
 
-theorem homotopy_lift (Continuous F₀ : Y → E) : ∃ Continuous F : Y × I → E, 
-  p ∘ F = f ∧ (fun y => F (y, t)) = F₀ := by 
+theorem homotopy_lift (Continuous f : Y × I → X) (IsCoveringMap p : E → X) (Continuous F₀ : Y → E) :
+  ∃ Continuous F : Y × I → E, p ∘ F = f ∧ (fun y => F (y, 0)) = F₀ := by 
     sorry
